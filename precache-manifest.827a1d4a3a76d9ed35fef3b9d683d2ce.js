ca3b36053382bf117013d87ceeb7f65aa0192273
dattaprasad-mundada <dattaprasad.mundada@pubmatic.com>
2019-05-19 12:50:36 +0530

    Add gh-pages for App

---
+++ precache-manifest.827a1d4a3a76d9ed35fef3b9d683d2ce.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d90ab5e736d9041f04feb6de9c2ff5b8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "80ded38677cdcfb10a6c",
+    "url": "/static/css/main.3baa5ac8.chunk.css"
+  },
+  {
+    "revision": "a70b5c9d20b4a3c425c1",
+    "url": "/static/js/2.6c230d06.chunk.js"
+  },
+  {
+    "revision": "80ded38677cdcfb10a6c",
+    "url": "/static/js/main.28ca2271.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- src/common/api.js
+++
@@ -1,3 +0,0 @@
-export const API = {
-    LIVE_STOCK_URL: 'ws://stocks.mnet.website/'
-};
